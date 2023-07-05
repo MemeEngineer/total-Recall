88a05ce825be6d571430e6fa29247b5ed3c91a5b
JWu <johnnyw131@gmail.com>
2023-07-05 19:08:30 -0400

    The Farm

--- w4d4.js
+++ w4d4.js
@@ -65,3 +65,18 @@ console.log(e === 'Kevin');
 console.log(a + b == c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
 console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
 console.log(48 !== '48');
+
+
+//======== The Farm ========
+
+const animal = "cow"
+
+if(animal === "cow"){
+    console.log("Mooooooooo")
+}else{
+    console.log("Hey! You're not a cow.")
+}
+
+//========== Driver's Ed =========
+
+
