e8e4b422ef85a2be5577a66f913dd78c51a8fad0
JWu <johnnyw131@gmail.com>
2023-07-05 16:30:36 -0400

    Variables & Datatypes

---
+++ w4d4.js
@@ -0,0 +1,38 @@
+// ======= JavaScript Review Lab =========
+
+//======== Variables & Datatypes =========
+
+/*
+How do we assign a value to a variable?
+1. const variable = value or let variable = value
+using an variable name and a = sign
+
+How do we change the value of a variable?
+2. reassign the value of the variable 
+variable = newValue
+
+How do we assign an existing variable to a new variable?
+3. Pass the value to the new variable by reference
+  
+let A = 123;
+let B = A;
+
+Remind me, what are the declare, assign, and Define?
+4. Declare tells what the datatype of the variables are going to be: string, Int, etc... has global scope, permanent and immutable.
+
+const A;
+
+Assign is giving the variable a value: 123, "hello", etc..
+const A = 123;
+
+Define is an expression object, no permanence and is mutable. It is referenced and can vanish from memory.
+
+What is pseudocoding and why should you do it?
+5. Pseudocoding is solving the problem in steps logically in terms you can break down before doing any code. If you can solve the problem with words/steps you already have 90% solved the problem before coding it helps people breakdown the problem into managable pieces. Coding is just putting the steps into action.
+
+What percentage of time should be spent thinking about how you're going to solve a problem vs acutally typing in code to solve it?
+6. spend more time solving the problem 90% and 10% coding because if you can solve it without coding then the rest will be easy to put the code to work.
+*/
+
+//======= Strings =========
+
